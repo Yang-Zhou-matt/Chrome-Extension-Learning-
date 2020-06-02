af58aefdfcf90f50c37975b2d84d264fe99d3d5c
Zhou-Yang-whatever <34389142+Zhou-Yang-whatever@users.noreply.github.com>
2020-06-01 20:42:05 -0700

    Create PageEvent.js

---
+++ PageEvent.js
@@ -0,0 +1,39 @@
+var contextMenuItem = {
+		"id": "spendMoney",
+		"title": "SpendMoney",
+		"contexts": ["selection"]
+};
+
+chrome.contextMenus.create(contextMenuItem);
+
+function isInt(value){
+	return !isNaN(value)&&
+			parseInt(Number(value)) == value &&
+			!isNaN(parseInt(value, 10));
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+	if(clikData.menuItemId == "spendMoney" && clickData.delectionText){
+		if(isInt(clickData.selectionText)){
+			chrome.storage.sync.get(["total", "limit"], function(budget){
+				var newTotal = 0;
+				if(budget.total){
+					newTotal += parseInt(budget.total);
+				}
+				newTotal += parseInt(clickdata.selectionText);
+				chrome.storage.sync.set({"total": newTotal}, function(){
+					if(newTotal >= budget.limit){
+						var notifOptions = {
+	    						type: "basic",
+	    						iconUrl:"icon48.png",
+	    						title: "Limit reached!",
+	    						message: "Uh,oh! Looks like you've reached your limit!"
+	    				};
+	    				chrome.notifications.create('limitNotif', notifOptions);
+
+					}
+				})
+			})
+		}
+	}
+})
